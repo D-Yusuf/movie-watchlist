262d53ed2ae86c7a6f8d6965837402124c0bbf37
D-Yusuf <alnasiriyusuf@gmail.com>
2023-06-25 16:15:46 +0300

    added dotenv

--- app/main.js
+++ app/main.js
@@ -1,3 +1,5 @@
 // document.getElementById('MOVIE-ID-HERE').addEventListener('change', ()=>{
 //   console.log(document.getElementById('MOVIE-ID-HERE').checked)
 // })
+const apiKey = import.meta.env.API_KEY
+console.log(apiKey)
