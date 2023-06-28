debe59e66c982e730180eb2f4cd8a6251408b1f6
D-Yusuf <alnasiriyusuf@gmail.com>
2023-06-28 07:22:48 +0300

    finished base app

--- app/main.js
+++ app/main.js
@@ -1,21 +1,36 @@
-const apiKey = import.meta.env.VITE_API_KEY;
-import starIcon from "./images/star-icon.svg";
-import addIcon from "./images/add-movie-icon.svg";
-import addIconDark from "./images/add-movie-icon-dark.svg";
-import removeIcon from "./images/remove-movie-icon.svg";
-import removeIconDark from "./images/remove-movie-icon-dark.svg";
-import {getImdb, getSearchedImdb, renderMovies ,changeCheckBoxIcon} from "./public/utils.js"
-export let movieCheckboxesId = [];
+
+import {
+  apiKey,
+  getImdb,
+  getSearchedImdb,
+  renderMovies,
+  changeCheckBoxIcon,
+  setCheckBoxes,
+  saveToLocalStorage
+} from "./public/utils.js";
+
+export let savedMoviesIdArr = [];
+export let checkedBoxesIdArr = [];
 export const moviesFromLocalStorage = JSON.parse(
-  localStorage.getItem("movieCheckBoxes")
+  localStorage.getItem("savedMovies")
+);
+export const checkBoxesFromLocalStorage = JSON.parse(
+  localStorage.getItem("savedMovies")
 );
 
+export const loadingScreen = document.getElementById("loading-screen");
 const searchForm = document.getElementById("search-form");
-const loadingScreen = document.getElementById("loading-screen");
 const searchPlaceholder = document.getElementById("search-placeholder");
-if (moviesFromLocalStorage) {
-  movieCheckboxesId = moviesFromLocalStorage
-  console.log(movieCheckboxesId);
+window.onload = getLocalStorageItems();
+
+function getLocalStorageItems(){
+  if (moviesFromLocalStorage) {
+    savedMoviesIdArr = moviesFromLocalStorage;
+  }
+  if (checkBoxesFromLocalStorage) {
+    checkedBoxesIdArr =  checkBoxesFromLocalStorage;
+  }
+  setCheckBoxes();
 }
 
 searchForm.addEventListener("submit", (e) => {
@@ -27,32 +42,29 @@ searchForm.addEventListener("submit", (e) => {
     .then((data) => {
       if (data.Response == "False") {
         searchPlaceholder.innerHTML = `<h1 class="text-[#DFDDDD] dark:text-[#2E2E2F] font-bold text-lg mt-2 max-w-xs text-center">
-          Unable to find what you're looking for. Please try another search.
-        </h1>`;
+      Unable to find what you're looking for. Please try another search.
+      </h1>`;
         loadingScreen.classList.add("hidden");
       } else {
         renderMovies(data.Search);
       }
     });
-  searchForm.reset();
 });
 
-document.addEventListener("click", (e) => {
+document.addEventListener("click", addOrRemoveToWatchlist);
+function addOrRemoveToWatchlist(e){
   if (e.target.dataset.id) {
-    changeCheckBoxIcon()
-    movieCheckboxesId = Array.from(
+    checkedBoxesIdArr = Array.from(
       document.querySelectorAll(`input[type="checkbox"]:checked`)
-    ).map((box) => {return box.id}); //gets id of checkboxes
-    saveToLocalStorage("movieCheckBoxes", movieCheckboxesId);
+      ).map((box) => {
+        return box.id;
+      }); //gets id of checkboxes
+    savedMoviesIdArr = checkedBoxesIdArr;
+    changeCheckBoxIcon();
+    saveToLocalStorage("savedMovies", savedMoviesIdArr);
+    saveToLocalStorage("checkedBoxes", checkedBoxesIdArr);
   }
-});
-function saveToLocalStorage(key, arr) {
-  // save to local storage
-  localStorage.setItem(key, JSON.stringify(arr));
+
 }
-fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=hello`)
-  .then((res) => res.json())
-  .then(async (data) => {
-    await renderMovies(data.Search);
-  });
+
 
--- app/public/watchlist.js
+++ app/public/watchlist.js
@@ -1,8 +1,72 @@
-import movieCheckboxId from "../main.js"
-import moviesFromLocalStorage from "../main.js"
-window.onload = async () =>{
-    // if (moviesFromLocalStorage && checkBoxesFromLocalStorage) {
-    //   savedMovies = moviesFromLocalStorage;
-    //   await renderMovies(savedMovies);
-    // }
+import starIcon from "../images/star-icon.svg";
+import addIcon from "../images/add-movie-icon.svg";
+import addIconDark from "../images/add-movie-icon-dark.svg";
+import removeIcon from "../images/remove-movie-icon.svg";
+import removeIconDark from "../images/remove-movie-icon-dark.svg";
+import { apiKey, setCheckBoxes, getSearchedImdb } from "./utils.js";
+import { moviesFromLocalStorage, checkBoxesFromLocalStorage } from "../main.js";
+import { checkedBoxesIdArr } from "../main.js";
+const savedMovies = document.getElementById("saved-movies");
+// setCheckBoxes()
+let moviesId = moviesFromLocalStorage;
+let checkedBoxes = checkBoxesFromLocalStorage;
+async function renderWatchlist() {
+    const searchedMovies = await getSearchedMovies()
+    let moviesHtml = ``
+    searchedMovies.forEach((movie) => {
+        const { Title, Genre, Plot, Poster, imdbRating, imdbID, Runtime } = movie;
+        moviesHtml += `
+          <div data-movie="${imdbID}" class="flex">
+          <img src="${Poster}" class="max-w-[6.5rem] max-h-[9.2rem]" alt="poster" />
+          <div class="ml-[1.31rem]">
+            <div class="flex items-center">
+              <h1 class="text-lg font-medium mr-2">${Title}</h1>
+              <span class="flex">
+                <img
+                  class="mr-1 w-[0.9375rem] h-[0.9375rem]"
+                  src="${starIcon}"
+                  alt="star"
+                />
+                <p class="text-xs">${imdbRating}</p>
+              </span>
+            </div>
+            <div class="flex mt-[0.69rem]">
+              <p class="sm:mr-[1.19rem] mr-3 text-xs">${Runtime}</p>
+              <p class="sm:mr-[1.19rem] mr-3 text-xs whitespace-normal"><span class="break-words">${Genre}</span></p>
+              <input hidden type="checkbox" data-id="${imdbID}" id="${imdbID}" />
+              <label for="${imdbID}" class="cursor-pointer text-xs gap-x-[0.31rem] flex flex-wrap sm:w-fit phone:ml-0 ml-auto justify-center items-center">
+                <img class="image-light" src="${addIcon}" alt="add" />
+                <img class="image-dark" src="${addIconDark}" alt="add" />
+                <p class="phone:block hidden">Watchlist</p>
+              </label>
+            </div>
+            <div class="flex mt-2">
+              <p class="text-[#6B7280] dark:text-[#A5A5A5] max-w-xs text-sm">
+                ${Plot}
+              </p>
+            </div>
+          </div>
+        </div>
+        <hr class="bg-[#E5E7EB] w-full my-[1.3rem]" />
+          `;
+      });
+      savedMovies.innerHTML = moviesHtml;
+      setCheckBoxes()
+}
+async function getSearchedMovies() {
+    let searchedMovies = [];
+    for (let id of moviesId) {
+      await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${id}`)
+        .then((res) => res.json())
+        .then((data) => searchedMovies.push(data));
+    }
+    return searchedMovies;
   }
+
+window.onload = ()=>{
+    if(moviesId.length){
+        renderWatchlist()
+
+    }
+}
+// console.log(moviesFromLocalStorage)
